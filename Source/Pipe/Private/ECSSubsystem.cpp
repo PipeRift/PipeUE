1f438ec12bccf365959bb6194c4c43b735c5a416
muit <muit@piperift.com>
2025-02-10 19:53:00 +0100

    Added ECS Subsystem

---
+++ Source/Pipe/Private/ECSSubsystem.cpp
@@ -0,0 +1,100 @@
+// Copyright 2015-2024 Piperift - All rights reserved
+
+#include "ECSSubsystem.h"
+
+#include "PipeUE.h"
+
+#include <Blueprint/BlueprintExceptionInfo.h>
+#include <Engine/Engine.h>
+#include <Engine/World.h>
+
+
+#define LOCTEXT_NAMESPACE "ECS"
+
+
+void UECSSubsystem::PostInitialize()
+{
+	Super::PostInitialize();
+	Ctx.SetStatic<UECSSubsystem*>(this);
+}
+
+void UECSSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+	Ctx.RemoveStatic<UECSSubsystem*>();
+}
+
+bool UECSSubsystem::DoesSupportWorldType(const EWorldType::Type WorldType) const
+{
+	switch (WorldType)
+	{
+		case EWorldType::Game:
+		case EWorldType::Editor:
+		case EWorldType::PIE:
+		case EWorldType::EditorPreview:
+		case EWorldType::GamePreview:
+			return true;
+	}
+	return false;
+}
+
+DEFINE_FUNCTION(UECSSubsystem::execGetComponent)
+{
+	P_GET_OBJECT(UECSSubsystem, ECS);
+	P_GET_STRUCT(FPipeId, Id);
+
+	// Read wildcard Value input.
+	Stack.MostRecentPropertyAddress = nullptr;
+	Stack.MostRecentPropertyContainer = nullptr;
+	Stack.StepCompiledIn<FStructProperty>(nullptr);
+
+	const FStructProperty* ValueProp = CastField<FStructProperty>(Stack.MostRecentProperty);
+	void* ValuePtr = Stack.MostRecentPropertyAddress;
+
+	P_FINISH;
+
+	if (!ValueProp || !ValuePtr)
+	{
+		if (FBlueprintCoreDelegates::IsDebuggingEnabled())
+		{
+			FBlueprintExceptionInfo ExceptionInfo(EBlueprintExceptionType::AbortExecution,
+				LOCTEXT(
+					"GetInvalidValueWarning", "Failed to resolve the Value for Get Instanced Struct Value"));
+
+			FBlueprintCoreDelegates::ThrowScriptException(P_THIS, Stack, ExceptionInfo);
+		}
+	}
+	else
+	{
+		P_NATIVE_BEGIN;
+		*(bool*) RESULT_PARAM = false;
+		if (ECS && ValueProp->Struct)
+		{
+			if (p::TypeId* TypeId = ECS->StructsToTypeIds.Find(ValueProp->Struct))
+			{
+				if (p::BasePool* Pool = ECS->GetContext().GetPool(*TypeId))
+				{
+					if (void* Value = Pool->TryGetVoid(Id))
+					{
+						ValueProp->Struct->CopyScriptStruct(ValuePtr, Value);
+						*(bool*) RESULT_PARAM = true;
+					}
+				}
+			}
+		}
+		P_NATIVE_END;
+	}
+}
+
+UECSSubsystem* UECSSubsystem::Get(const UWorld* World)
+{
+	return UWorld::GetSubsystem<UECSSubsystem>(World);
+}
+
+UECSSubsystem* UECSSubsystem::Get(const UObject* ContextObject)
+{
+	UWorld* World = GEngine->GetWorldFromContextObject(ContextObject, EGetWorldErrorMode::ReturnNull);
+	return UECSSubsystem::Get(World);
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/Pipe/Public/ECSSubsystem.h
@@ -0,0 +1,57 @@
+// Copyright 2015-2024 Piperift - All rights reserved
+
+#pragma once
+
+#include <CoreMinimal.h>
+#include <PipeECS.h>
+#include <Subsystems/WorldSubsystem.h>
+
+#include "ECSSubsystem.generated.h"
+
+
+UCLASS(Blueprintable, DisplayName = "ECS")
+class PIPE_API UECSSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+	p::EntityContext Ctx;
+
+	p::TMap<UScriptStruct*, p::TypeId> StructsToTypeIds;
+
+public:
+	void PostInitialize() override;
+	void Deinitialize() override;
+
+	p::EntityContext& GetContext()
+	{
+		return Ctx;
+	}
+
+	const p::EntityContext& GetContext() const
+	{
+		return Ctx;
+	}
+
+protected:
+	bool DoesSupportWorldType(const EWorldType::Type WorldType) const override;
+
+	UFUNCTION(BlueprintCallable, CustomThunk, Category = "Pipe|ECS", meta = (CustomStructureParam = "Value"))
+	static UPARAM(DisplayName = "Exists") bool GetComponent(UECSSubsystem* ECS, FPipeId Id, int32& Value);
+	DECLARE_FUNCTION(execGetComponent);
+
+public:
+	static UECSSubsystem* Get(const UWorld* World);
+	static UECSSubsystem* Get(const UObject* ContextObject);
+};
+
+
+inline p::EntityContext* GetECS(const UWorld* World)
+{
+	auto* ECSSubsystem = UECSSubsystem::Get(World);
+	return ECSSubsystem ? &ECSSubsystem->GetContext() : nullptr;
+}
+inline p::EntityContext* GetECS(const UObject* ContextObject)
+{
+	auto* ECSSubsystem = UECSSubsystem::Get(ContextObject);
+	return ECSSubsystem ? &ECSSubsystem->GetContext() : nullptr;
+}
