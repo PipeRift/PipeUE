14e009666228cb04ba466337a539f90c0c150f0a
muit <muit@piperift.com>
2026-01-05 19:12:14 +0100

    Added inspection of vector types

--- Source/Pipe/Public/PipeUEDebug.h
+++ Source/Pipe/Public/PipeUEDebug.h
@@ -21,6 +21,16 @@ namespace p
 				value = FName{p::ToUE(pValue)};
 			}
 		});
+		P_DECLARE_COMMON_VALUE_TYPEINSPECTION(
+			FIntPoint, { ImGui::InputScalarN("##value", ImGuiDataType_S32, &value, 2); });
+		P_DECLARE_COMMON_VALUE_TYPEINSPECTION(
+			FIntVector, { ImGui::InputScalarN("##value", ImGuiDataType_S32, &value, 3); });
+		P_DECLARE_COMMON_VALUE_TYPEINSPECTION(
+			FVector, { ImGui::InputScalarN("##value", ImGuiDataType_Double, &value, 3); });
+		P_DECLARE_COMMON_VALUE_TYPEINSPECTION(
+			FVector3f, { ImGui::InputScalarN("##value", ImGuiDataType_Float, &value, 3); });
+		P_DECLARE_COMMON_VALUE_TYPEINSPECTION(
+			FVector2D, { ImGui::InputScalarN("##value", ImGuiDataType_Float, &value, 2); });
 	}
 }	 // namespace p
 #endif
