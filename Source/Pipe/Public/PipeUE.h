12e2b6e07b51b58955bcfa664435cbda47f34a42
muit <muit@piperift.com>
2026-02-21 15:19:22 +0100

    Support inline arrays to ue

--- Source/Pipe/Public/PipeUE.h
+++ Source/Pipe/Public/PipeUE.h
@@ -219,8 +219,8 @@ namespace p
 		}
 		return MoveTemp(Results);
 	}
-	template <typename T, typename U = decltype(ToUE(std::declval<const T&>()))>
-	inline ::TArray<U> ToUE(const p::TArray<T>& Values)
+	template <typename T, int32 N, typename U = decltype(ToUE(std::declval<const T&>()))>
+	inline ::TArray<U> ToUE(const p::TInlineArray<T, N>& Values)
 	{
 		return ToUE(p::TView<T>{Values});
 	}
